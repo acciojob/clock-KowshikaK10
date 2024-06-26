be47972d6d404a8513baab1a621922b6d2b3f4eb
KowshikaK10 <139222757+KowshikaK10@users.noreply.github.com>
2024-06-26 18:54:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let time=document.getElementById('timer');
+
+function clock(){
+	let dateData=new Date();
+	let date=dateData.getDate();
+	let month=dateData.getMonth();
+	let year=dateData.getFullYear();
+	//time
+	let hour=dateData.getHours();
+	let min=dateData.getMinutes();
+	let sec=dateData.getSeconds();
+	let ampm;
+	hour=(hour>12) ? hour-12 : hour;
+	(hour>12) ? ampm='PM' : ampm='AM';
+	time.innerHTML=`${date}/${month}/${year}, ${hour}:${min}:${sec} ${ampm}`
+}
+clock();
